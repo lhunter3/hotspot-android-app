0ae59f4fe145d2c4611794521e378bb2b7fa585d
Lucas Hunter <lucasbhunter@gmail.com>
2023-10-22 02:02:21 -0300

    various changes

--- app/src/main/java/unb/cs2063/hotspots/ui/camera/CameraFragment.kt
+++ app/src/main/java/unb/cs2063/hotspots/ui/camera/CameraFragment.kt
@@ -23,6 +23,7 @@ import android.view.animation.RotateAnimation
 import android.widget.ProgressBar
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
